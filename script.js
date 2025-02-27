d5b361a73be46da56a6071e57f7ab383a9f325eb
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-02-27 19:29:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // write js code here if required
+let timeOutput = document.getElementById('timer')
+let date = new Date()
+
+
+let month = date.getUTCMonth()
+let year = date.getUTCFullYear()
+let day = date.getUTCDay()
+
+let hours = date.getUTCHours();
+let min = date.getUTCMinutes();
+let sec = date.getUTCSeconds();
+
+let timeToday = (`${day}/${month}/${year},${hours}:${min}:${sec}`)
+
+timeOutput.innerText = timeToday
+
